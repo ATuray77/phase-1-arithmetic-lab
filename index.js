4355467e1002678422a2e6a78df45c4d81ea0539
Aruna Turay <tura_arun@yahoo.com>
2023-03-16 17:30:37 -0400

    math

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 2) + 1;
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3 % num4
+
+let max = Math.max(2, 4, 20);
